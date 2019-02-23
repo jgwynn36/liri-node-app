4faa612adb75c31584917ae0e453e46019338608
Jacqueline Gwynn <jacquelinegwynn@Jacquelines-MacBook-Air.local>
2019-02-23 13:29:37 -0500

    concert-this working

--- liri.js
+++ liri.js
@@ -1,5 +1,57 @@
 require("dotenv").config();
+const Spotify = require("node-spotify-api");
 
-var keys = require("./keys.js");
+const moment = require ("moment"); 
+const axios = require("axios");
+const fs = require("fs");
 
-var spotify = new Spotify(keys.spotify);
+const keys = require("./keys.js");
+
+// const spotify = new Spotify(keys.spotify);
+const artist = process.argv.slice(3).join(' '); 
+const song = " ";
+const type = process.argv[2];
+
+const liri = function (type, artist) {
+    switch (type) {
+        case "concertList":
+        concertList(artist); 
+            break;
+        case "songInfo":
+            // code block
+            break;
+        case "movieInfo":
+            // code block
+
+            break;
+        case "random":
+            //code block
+            break;
+        default:
+           console.log ("Sorry something went wrong with your search!!"); 
+    }
+
+    
+}
+
+const concertList = function (artist) {
+    console.log(artist)
+    console.log('concertList')
+    const divider = "\n------------------------------------------------------------\n\n";
+    
+        const URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        axios.get(URL).then(function (response) {
+            const jsonData = response.data;
+
+            console.log(JSON.stringify(jsonData, null, 2));
+
+            const showList = [
+                "Venue Name: " + jsonData.venue.name,
+                "Venue Location: " + jsonData.venue.city + jsonData.venue.country,
+                "Date: " + jsononData.datetime
+            ]
+            console.log(showList); 
+        });
+}
+
+liri(type, artist)
