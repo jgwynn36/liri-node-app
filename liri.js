d5ab3c533bf1146cb66ca8b27ca76fa60e3bef26
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-02-24 00:35:05 -0500

    movie1

--- liri.js
+++ liri.js
@@ -10,8 +10,9 @@ const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
 const type = process.argv[2];
 const artist = process.argv.slice(3).join(' ');
-const input = process.argv[4];
+const input = process.argv.slice(4).join(' ');
 const song = process.argv.slice(5).join(' ');
+const divider = "\n------------------------------------------------------------\n";
 // const dataInput = dataArray[1];
 // const dataArray = data.split(',');
 
@@ -22,12 +23,9 @@ const liri = function (type, artist) {
             break;
         case "songInfo":
             songInfo(song);
-            // input = dataInput; 
-            // song(); 
             break;
         case "movieInfo":
-            // code block
-
+            movieInfo(movie);
             break;
         case "random":
             //code block
@@ -35,13 +33,11 @@ const liri = function (type, artist) {
         default:
             console.log("Sorry something went wrong with your search!!");
     }
-
 }
 
 const concertList = function (artist) {
     console.log(artist)
     console.log('concertList')
-    const divider = "\n------------------------------------------------------------\n\n";
 
     const URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get(URL).then(function (response) {
@@ -56,6 +52,10 @@ const concertList = function (artist) {
                 console.log("Concert Date: " + moment(jsonData[i].datetime).format('MM/DD/YYYY'));
                 console.log(divider);
 
+                fs.appendFile('log.txt', songData, function (error) {
+                    if (error) throw error;
+                });
+
             });
         }
     });
@@ -64,11 +64,11 @@ const concertList = function (artist) {
 const songInfo = function (song) {
     console.log('Searching for your song...');
     if (song === undefined) {
-        song = 'Happy'
+        song = 'The Sign'
     } else {
         song = input;
     }
-    console.log("Your Search information is .............n/n");
+    console.log("Your Search information is .............");
     spotify.search({
             type: 'track',
             query: song
@@ -78,9 +78,10 @@ const songInfo = function (song) {
                 console.log(`Artist(s): ${data.tracks.items[0].artists[0].name}`);
                 console.log(`Song: ${data.tracks.items[0].name}`);
                 console.log(`Link: ${data.tracks.items[0].external_urls.spotify}`);
-                console.log(`Album: ${data.tracks.items[0].album.name}`)
+                console.log(`Album: ${data.tracks.items[0].album.name}`);
+                console.log(divider);
 
-                let songData = `\nUsed songInfo to find:  \nArtist: ${data.tracks.items[0].artist[0].name} \nSong Name: ${data.tracks.items[0].name} \nSpotify Preview Link: ${data.tracks.items[0].external_urls.spotify} n\Album: {$data.tracks.items[0].album.name}n\---------------------------------`
+                const songData = `\nUsed songInfo to find:  \nArtist: ${data.tracks.items[0].artist[0].name} \nSong Name: ${data.tracks.items[0].name} \nSpotify Preview Link: ${data.tracks.items[0].external_urls.spotify} n\Album: {$data.tracks.items[0].album.name}n\---------------------------------`
 
                 fs.appendFile('log.txt', songData, function (error) {
                     if (error) throw error;
@@ -88,5 +89,34 @@ const songInfo = function (song) {
             }
         });
 }
- 
-liri(type, artist, song); 
+
+const movieInfo = function (movie) {
+    console.log("Searching for your movie ....");
+    if (movie === undefined) {
+        movie = 'Mr. Nobody'
+    } else {
+        movie = input;
+    }
+    const URL = "http://www.omdbapi.com/?apikey=trilogy&" + movie;
+
+    axios.get(URL).then(function (response) {
+        const jsonData = response.data;
+
+        console.log(JSON.stringify(jsonData, null, 2));
+        for (let i = 0; i < jsonData.length; i++) {
+            jsonData.forEach(element => {
+                console.log("Title of the Movie: " + jsonData[i].venue.name);
+                console.log("Venue Location: " + jsonData[i].venue.country);
+                console.log("Concert Date: " + moment(jsonData[i].datetime).format('MM/DD/YYYY'));
+                console.log(divider);
+
+                fs.appendFile('log.txt', songData, function (error) {
+                    if (error) throw error;
+                });
+
+            });
+        }
+    });
+}
+
+liri(type, artist, song);
